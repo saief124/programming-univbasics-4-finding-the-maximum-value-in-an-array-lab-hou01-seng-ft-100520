256c3be9dae1652189c18dfb67fadf2de11bed0c
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-15 07:23:07 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,13 @@
 def find_max_value(array)
-  # Add your solution here
+  count = 0
+  while count<array.length do
+    if {
+      array[count]>array[count+1]
+      return count
+    else
+      return count+1
+    }
+    end
+    count= count + 1
+  end
 end
