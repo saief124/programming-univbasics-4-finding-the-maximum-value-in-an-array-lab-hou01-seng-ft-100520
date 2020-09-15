fb9bd75f07542db1e993aa1b6ed444cd549cc8c9
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-15 07:28:06 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,13 +1,12 @@
 def find_max_value(array)
   count = 0
+  max_value=-1
   while count<array.length do
     if
-      array[count]>array[count+1]
-      return count
-    else
-      return count+1
-
+      max_value<array[count]
+      max_value=array[count]
     end
     count= count + 1
   end
+  max_value
 end
